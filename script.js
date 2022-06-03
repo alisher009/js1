76c9dc499ace3e567e7851aa55ead27b77a576a1
alisher <chexramchik@gmail.com>
2022-06-03 17:17:03 +0500

    js

---
+++ script.js
@@ -0,0 +1,20 @@
+let name = 'Jon'
+let admin = name
+console.log(admin);
+
+
+let balanc = 25000
+// Затраты 
+let car = 4480
+let food = 890
+// Доход 
+let invoice = 5500
+let stock = 4200 
+// Итого
+let totalBalance = 0
+
+
+totalBalance =  balanc - car-food +invoice+stock;
+console.log(totalBalance);
+
+
