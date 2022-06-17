559be29fdc916fbbd19fb863f9412a32e39cb768
alisher <chexramchik@gmail.com>
2022-06-17 19:41:41 +0500

    dz2

--- script.js
+++ script.js
@@ -1,60 +1,19 @@
+let arr = [ 'alish','amir', 'amin', 'zarina', 'shoxrux ', 'kumush', 'amina', 'xoji', 'shox', 'ula', 'artyom'] 
+let e = Math.floor(Math.random() *100)
 
-let name = prompt('Как вас зовут?')
-let money = 10000
-let account = 7777
+let a = confirm(`Добро пожаловать в наш  ресторан: 
+в наш ресторан можно зойти только по записи: 
+Вы бронировали места? `)
 
-name = name[0].toUpperCase() + name.slice(1)
-
-
-if(name === 'Alex'){
-    let account = +prompt("Номер счетa?")
-    if(account == account){
-        let accountMoney = +prompt('Сколько обналичить?')
-        if(accountMoney <= money){
-            let totolmoney = money - accountMoney
-            alert(`Операция успешно завершина   Снято: ${accountMoney}
-            Осталось: ${totolmoney}`);
-         
-        } else {
-            alert( " На счету, Недостаточно средст. ");
-        } 
-    } else {
-        alert('Пользователь не найден: проверте и повторите попытку')
-    }
-} else {
-    alert('Пользователь не найден: проверте и повторите попытку' )
+if(a === true) {
+    let name = prompt('Как вас зовут?') 
+if(arr.includes(`${name}`)) {
+    alert(`Добро пожаловать ${name} 
+     Ваш столик готов  `+ e )
+} else  {
+   alert('Увы вас нет в списке гостей');
 }
-
-
-
-// // Задание.2
-// let name = prompt('как вас зовут?')
-
-// if (name[0].toLowerCase() === 'a') {
-//     console.log('добро пожаловать ');
-//     let age = prompt('Ваш возраст')
-//     if ( age >= 20 && age <= 40); {
-//         console.log('добро пожаловать ');
-
-
-//         let accout = prompt('вход больше 100$ ')
-//         if (accout >= 100) {
-//             console.log('Все Отлично');
-
-//             let b = prompt('вместимость 10 человек')
-
-//             if (b > 0 && b <= 10) {
-//                 console.log('готово')
-//             } else {
-//                 console.log('много людей');
-//             }
-
-//         } else {
-//             console.log('На счету недостаточно средств Повторите Попытку');
-//         }
-//     }
-//  } else {
-//     console.log('мы за росизм');
-// } 
-
+} else {
+   alert('Вам нужно забронировать столик')
+}
 
