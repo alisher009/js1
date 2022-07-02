3119f2a58a03d457ba90f1514ace7202667baaa2
alisher <chexramchik@gmail.com>
2022-07-02 14:13:38 +0500

    DZ

--- script.js
+++ script.js
@@ -1,198 +1,35 @@
-name = prompt('Введите имя', '')
-let people = [
+let users = [
     {
-        id: Math.random(),
-        name: 'Alisher',
-        carSpends: 10,
-        budget: 400,
-        expenses: [
-            {
-                title: 'food',
-                total: 150
-            },
-            {
-                title: 'friends',
-                total: 30
-            },
-            {
-                title: 'girlfriend',
-                total: 120
-            },
-        ]
+        name: 'blblbl'
     },
     {
-        id: Math.random(),
-        name: 'Muxammad',
-        carSpends: 100,
-        budget: 1000,
-        expenses: [
-            {
-                title: 'food',
-                total: 300
-            },
-            {
-                title: 'friends',
-                total: 500
-            },
-            {
-                title: 'girlfriend',
-                total: 10
-            },
-        ]
+        name: 'bfbfbfb'
     },
     {
-        id: Math.random(),
-        name: 'daler aka',
-        carSpends: 10,
-        budget: 900,
-        expenses: [
-            {
-                title: 'food',
-                total: 300
-            },
-            {
-                title: 'friends',
-                total: 300
-            },            {
-                title: 'girlfriend',
-                total: 300
-            },
-        ]
+        name: 'fvfvfv'
     },
     {
-        id: Math.random(),
-        name: 'shoxrux',
-        carSpends: 10,
-        budget: 500,
-        expenses: [
-            {
-                title: 'food',
-                total: 200
-            },
-            {
-                title: 'friends',
-                total: 150
-            },
-            {
-                title: 'girlfriend',
-                total: 215
-            },
-        ]
+        name: 'fvfvfv'
     },
     {
-        id: Math.random(),
-        name: 'Amir',
-        carSpends: 10,
-        budget: 100,
-        expenses: [
-            {
-                title: 'food',
-                total: 40
-            },
-            {
-                title: 'friends',
-                total: 50
-            },
-            {
-                title: 'girlfriend',
-                total: 0
-            },
-        ]
+        name: 'yufvfvfv'
     },
     {
-        id: Math.random(),
-        name: 'Amina',
-        carSpends: 5,
-        budget: 1000,
-        expenses: [
-            {
-                title: 'food',
-                total: 200
-            },
-            {
-                title: 'friends',
-                total: 250
-            },
-            {
-                title: 'girlfriend',
-                total: 0
-            }
-        ]
+        name: 'lfvfps'
     },
     {
-        id: Math.random(),
-        name: 'Amin',
-        carSpends: 10,
-        budget: 300,
-        expenses: [
-            {
-                title: 'food',
-                total: 150
-            },
-            {
-                title: 'friends',
-                total: 100
-            },
-            {
-                title: 'girlfriend',
-                total: 50
-            },
-        ]
-    },
-    {
-        id: Math.random(),
-        name: 'Zarina',
-        carSpends: 10,
-        budget: 400,
-        expenses: [
-            {
-                title: 'food',
-                total: 200
-            },
-            {
-                title: 'friends',
-                total: 80
-            },
-            {
-                title: 'shopping',
-                total: 120
-            },
-        ]
+        name: 'skriptonit'
     }
 ]
 
-
-let poor = []
-let rich = []
-
-
-// for(let item of people){
-
-//     let x = item.carSpends * item.budget / 100
-//     let totalExp = item.total = item.expenses.reduce((a,b) => {
-//        return a + b.total
-//     }, 0)
-//     totalExp = item.budget - (totalExp + x)
-//     console.log(x, totalExp, item.name);
-
-//         if(totalExp < -0){
-//             poor.push(item)
-//         }else if (totalExp >= 0){
-//             rich.push(item)
-//         }
-    
-// }
-// console.log(poor);
-// console.log(rich);
-
-
-
-
-let personsFiltered = []
-people.forEach(people => {
-	if (people.name !== name) {   
-		personsFiltered.push(people)
-	}
-})
-console.log(personsFiltered)
-
+function addAge(users) {
+    let sum = 0
+    for (let user of users) {
+        user.age = Math.floor(Math.random() * 50)
+        sum += user.age
+    }
+    return sum / users.length
+}
+addAge(users)
+console.log(users);
+console.log('средний возраст', Math.round(addAge(users)))
