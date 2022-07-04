44050d07837648658202a7439a80adf4a6f2a9ec
alisher <chexramchik@gmail.com>
2022-07-04 22:05:03 +0500

    dz

--- script.js
+++ script.js
@@ -1,35 +1,127 @@
-let users = [
+let arr = [
     {
-        name: 'blblbl'
+        id: Math.random(),
+        name: 'hrhr',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
     },
     {
-        name: 'bfbfbfb'
+        id: Math.random(),
+        name: 'rhrh',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
     },
     {
-        name: 'fvfvfv'
+        id: Math.random(),
+        name: 'hhhr',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
     },
     {
-        name: 'fvfvfv'
+        id: Math.random(),
+        name: 'hhh',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
     },
     {
-        name: 'yufvfvfv'
+        id: Math.random(),
+        name: '44',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
     },
     {
-        name: 'lfvfps'
+        id: Math.random(),
+        name: 't4',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
     },
     {
-        name: 'skriptonit'
-    }
+        id: Math.random(),
+        name: 'Imgrtgran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'egerg',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Magrexmugegeged',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxrgergemud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'cbgnfgn',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
 ]
 
-function addAge(users) {
-    let sum = 0
-    for (let user of users) {
-        user.age = Math.floor(Math.random() * 50)
-        sum += user.age
+let categories = [
+    {
+        course: ' SMM',
+        count: 0
+    },
+    {
+        course: 'PROGRAMMING',
+        count: 0
+    },
+    {
+        course: '     3DSMAX',
+        count: 0
+    },
+    {
+        course: ' DIZAYN',
+        count: 0
+    },
+    {       
+        course: '   BACKEND',
+        count: 0
+    },
+]
+
+// Найти количество людей одного типа из массива и сохранить их в массив категории 
+// В массиве категорий специально допущены ошибки (исходный массив менять нельзя) 
+
+for(let item of arr ) {
+    for(let item2 of categories) {
+        if( item.info.faculity.toLocaleLowerCase() === item2.course.trim().toLocaleLowerCase() ){
+            item2.count++
+        }
+       
     }
-    return sum / users.length
 }
-addAge(users)
-console.log(users);
-console.log('средний возраст', Math.round(addAge(users)))
+
+console.log(categories);
